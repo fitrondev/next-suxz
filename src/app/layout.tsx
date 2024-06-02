9eb9a869464fabd829984f7df6ad7301dd42cd25
fitronansori <fitronansori7@gmail.com>
2024-06-02 21:43:47 +0800

    chore: Add layout component with metadata and theme provider

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/provider/theme-provider";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+});
+
+export const metadata: Metadata = {
+  title: "Suxz",
+  description: "Online learning platform with international curriculum.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={manrope.variable}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
