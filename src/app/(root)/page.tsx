a9cdf17404fc943da75136a8d35083d7ee2c4f89
fitronansori <fitronansori7@gmail.com>
2024-06-03 11:25:02 +0800

    feat: Add Learning component to Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./_components/Hero";
+import Learning from "./_components/Learning";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Learning />
     </>
   );
 }
