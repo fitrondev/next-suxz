e856ef94cdcea3a34f4684b3e8f8f6cfdf9c54ce
fitronansori <fitronansori7@gmail.com>
2024-06-03 11:24:55 +0800

    feat: Add Learning component

---
+++ src/app/(root)/_components/Learning/LearningCard.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+const LearningCard = ({
+  title,
+  description,
+  img,
+  index,
+}: {
+  title: string;
+  description: string;
+  img: string;
+  index: number;
+}) => {
+  return (
+    <Card
+      className={cn(
+        "w-[250px] h-[330px]",
+        index === 2 && "bg-primary/70 text-white"
+      )}>
+      <CardContent className="size-full rounded-lg p-0">
+        <div className="w-full h-[180px] bg-gray-200 dark:bg-foreground flex items-center justify-center rounded-t-lg">
+          <div className="relative size-40">
+            <Image src={img} alt="" fill className="size-20 object-contain" />
+          </div>
+        </div>
+
+        <div className="pt-4 px-4 space-y-2">
+          <div className="space-y-2">
+            <h4 className="text-xl font-bold">{title}</h4>
+
+            <p
+              className={cn(
+                "text-sm text-muted-foreground",
+                index === 2 && "text-white"
+              )}>
+              {description}
+            </p>
+          </div>
+
+          <div>
+            <Button size={"sm"} className="w-full text-sm" asChild>
+              <Link href="/learning/id">See Class</Link>
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+export default LearningCard;
---
+++ src/app/(root)/_components/Learning/index.tsx
@@ -0,0 +1,32 @@
+import Title from "@/components/Title";
+import { learningData } from "@/constants";
+import LearningCard from "./LearningCard";
+
+const Learning = () => {
+  return (
+    <section className="bg-[#F8F8FF] dark:bg-gray-900 py-10">
+      <div className="container space-y-16">
+        <div>
+          <Title
+            section="Learnings"
+            title="Online Learning Platform International Curriculum"
+            description="Study and understand the material class is more relaxed without time limit."
+          />
+        </div>
+
+        <div className="flex flex-col sm:flex-row flex-wrap items-center lg:items-start justify-center lg:justify-between gap-5 sm:gap-10 lg:gap-0">
+          {learningData.map((item, index) => (
+            <LearningCard
+              key={index}
+              index={index}
+              title={item.title}
+              description={item.description}
+              img={item.img}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Learning;
