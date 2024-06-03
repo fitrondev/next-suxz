804142d0f16c9fd2a0ca04876778edcd08db5b84
fitronansori <fitronansori7@gmail.com>
2024-06-03 11:24:07 +0800

    feat: Add learningData constant for course options

---
+++ src/constants/index.ts
@@ -0,0 +1,25 @@
+export const learningData = [
+  {
+    title: "Starter",
+    description: "Free class access for that start learning code and design.",
+    img: "/l1.svg",
+  },
+  {
+    title: "Premium",
+    description:
+      "Advanced material from the Starter & learn to build projects.",
+    img: "/l2.svg",
+  },
+  {
+    title: "Superstar",
+    description:
+      "Materials for experts who have completed the premium package.",
+    img: "/l3.svg",
+  },
+  {
+    title: "Bootcamp",
+    description:
+      "Informatics science training program with the latest material.",
+    img: "/l4.svg",
+  },
+];
