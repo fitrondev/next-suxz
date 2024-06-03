779424a7b76e2fa32b19e0d0b33c9cb20b8bc843
fitronansori <fitronansori7@gmail.com>
2024-06-03 21:19:01 +0800

    feat: Add StartSection component to Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,6 +2,7 @@ import About from "./_components/About";
 import Company from "./_components/Company";
 import Hero from "./_components/Hero";
 import Learning from "./_components/Learning";
+import StartSection from "./_components/StartSection";
 import Testimonials from "./_components/Testimonials";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Company />
       <Testimonials />
+      <StartSection />
     </>
   );
 }
